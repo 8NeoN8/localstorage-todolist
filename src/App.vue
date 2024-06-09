71334d5da742bad399eb50a9f43d04619365e0d4
AGDevs <74154297+8NeoN8@users.noreply.github.com>
2024-06-09 18:03:38 -0400

    Update: implement delete operation, implement operation notifications, finish tag dupe filtering. Soon: Tag sorting & display, final styles

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,35 @@
 <template>
   <div class="container">
     <div class="notes-and-options">
-      <h1 class="header">LOCAL TODO LIST</h1>
+      <dialog class="confirm-delete-dialog" :open="showConfirm">
 
+        <div class="confirm-text">
+          Are you sure you want to delete this note?
+        </div>
+
+        <div class="confirm-button">
+          <button class="button-yes" @click="deleteNote(); showConfirm = false">Yes</button>
+          <button class="button-no" @click="noteToDelete = null, showConfirm = false">No</button>
+        </div>
+
+      </dialog>
+      <h1 class="header">LOCAL TODO LIST</h1>
+      <select name="tag-select" class="tag-select" @change="test()">
+        <option value="" selected></option>
+        <template v-for="(tag, index) in globalTags" :key="index">
+          <option :value="tag"> {{tag}}</option>
+        </template>
+      </select>
       <ul class="note-list">
         <template v-for="(note, index) in notes" :key="index">
 
-          <div class="listed-note" @click="openNote(index)">
-            {{ note.id }}
-            <br>
-            {{ index }}
-            <br>
-            {{ note.title }}
+          <div class="listed-note">
+            <div class="note-title" @click="openNote(index)">
+              {{ note.title }}
+            </div>
+            <button class="delete-button" @click="confirmNoteDelete(note)">
+              &#x1F5D1;
+            </button>
           </div>
 
         </template>
@@ -35,6 +53,15 @@
       </div>
 
     </div>
+
+    <div v-if="showNotifs" class="notifications" :class="{'error': hasError, 'safe': isGood}">
+      <div class="notif-text">
+        {{ notifMsg }}
+      </div>
+      <div class="notif-close">
+        &#x274c;
+      </div>
+    </div>
   </div>
 </template>
 
@@ -43,6 +70,7 @@ export default{
   data() {
     return {
       notes:[],
+      filteredNotes: [],
       noteData:{
         id: null,
         exists: false,
@@ -50,8 +78,14 @@ export default{
         content: null,
         tags: null,
       },
-      globalTags: null,
-      isOpenNote: false
+      noteToDelete: null,
+      globalTags: [],
+      isOpenNote: false,
+      showNotifs: false,
+      notifMsg: 'Note Saved!',
+      hasError: false,
+      isGood: true,
+      showConfirm: false
     }
   },
   computed:{
@@ -62,7 +96,6 @@ export default{
   },
   mounted() {
     this.getNotes()
-    console.log(this.notes);
   },
   methods: {
     setAndGet(){
@@ -76,9 +109,31 @@ export default{
       if(!this.notes) localStorage.setItem('noteList', [])
 
       if(this.notes) this.notes = JSON.parse(this.notes)
+
+      this.globalTags = []
+
+      this.notes.forEach(note => {
+
+        if(note.tags == null || note.tags == '') return
+
+        let noteTags = note.tags
+        noteTags = noteTags.split(' ')
+        for (let i = 0; i < noteTags.length; i++) {
+          this.globalTags.push(noteTags[i])  
+        }
+      });
+
+      this.globalTags = this.globalTags.filter((value, index) => this.globalTags.indexOf(value) === index)
     },
     saveNote(){
       
+      if(
+        this.noteData.title == null || this.noteData.title == ''
+      ){
+        this.fillNotif(true)
+        return
+      }
+
       if(!this.noteData.exists){
         this.noteData = {
           ...this.noteData, 
@@ -94,19 +149,45 @@ export default{
         this.notes[this.notes.indexOf(this.noteData)] = this.noteData
       }
       
+      this.fillNotif()
 
       this.setAndGet()
     },
-    deleteNote(note){
-      this.notes.splice(this.notes.indexOf(note), 1)
+    fillNotif(error = false, ok = true){
+
+      this.notifMsg = 'Nota Guardada!'
+      this.hasError = false
+      this.isGood = true
+
+      if(error){
+        this.notifMsg = 'Titulo de Nota Requerido!'
+        this.hasError = true
+        this.isGood = false
+      }
+
+
+      this.showNotifs = true
+      setTimeout(() => {
+        this.showNotifs = false
+      }, 3000);
+    },    
+    confirmNoteDelete(note){
+      this.showConfirm = true
+      this.noteToDelete = note
+    },
+    deleteNote(){
+      this.notes.splice(this.notes.indexOf(this.noteToDelete), 1)
+      this.notes.filter((note) => {
+        return note.id != this.noteToDelete.id
+      })
 
       this.setAndGet()
     },
     openNote(index){
       this.noteData = this.notes[index]
     },
-    log(note, saveNote = false, deleteNote = false){
-
+    test(){
+      console.log('a');
     }
   },
 
